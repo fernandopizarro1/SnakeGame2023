70ea99e6c4dcdd08d8a28096ae7ad857aa207e95
david-baez-bravo <david.baez3@upr.edu>
2023-04-26 04:09:09 -0400

    Entity, StaticEntity, DynamicEntity Classes

---
+++ src/Entities/Entities.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "ofMain.h"
+
+class Entity {
+    protected:
+        int x, y, w, h;
+        bool remove = false;
+        int ticks = 0;
+        ofColor color;
+    
+    public:
+        Entity(int x, int y, int w, int h, ofColor color) {
+            this->x = x; 
+            this->y = y;
+            this->w = w;
+            this->h = h;
+            this->color = color;
+        };
+
+        virtual void update() = 0;
+        virtual void draw() = 0;
+};
+
+
+class StaticEntity : public Entity {
+    public:
+        StaticEntity(int x, int y, int w, int h, ofColor color) : Entity(x, y, w, h, color) {};
+        void update() override {};
+        void draw() override;
+};
+
+class DynamicEntity : public Entity {
+    public:
+        void update() override;
+        void draw() override;
+};
